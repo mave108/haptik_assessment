f0666d9e3f4f8d3c08c77d667ae6d0423c04c994
mave108 <rakeshkumar8900@gmail.com>
2019-01-22 18:45:04 +0530

    production build

---
+++ docs/precache-manifest.7ab7fbc957ea0ff699521eda2578069a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3852ac62b4b1a76c0144",
+    "url": "/haptik_assessment/static/js/runtime~main.3852ac62.js"
+  },
+  {
+    "revision": "f4c1dac79a97e1fdd975",
+    "url": "/haptik_assessment/static/js/main.f4c1dac7.chunk.js"
+  },
+  {
+    "revision": "7f06190ea708a72174a7",
+    "url": "/haptik_assessment/static/js/1.7f06190e.chunk.js"
+  },
+  {
+    "revision": "f4c1dac79a97e1fdd975",
+    "url": "/haptik_assessment/static/css/main.b13b3928.chunk.css"
+  },
+  {
+    "revision": "a16aa71303c4243e6392b586f54725de",
+    "url": "/haptik_assessment/index.html"
+  }
+];
